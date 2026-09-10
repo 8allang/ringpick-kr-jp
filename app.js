4ef3fcc264ef30655d51009854624b651d562850
8allang <aabc77711@gmail.com>
2026-09-11 06:07:56 +0900

    remove duplicate ranking medals

--- app.js
+++ app.js
@@ -1486,9 +1486,9 @@ function renderTopSavingsRanking() {
 
   const top3 = evaluated.slice(0, 3);
   const medals = [
-    { title: '🥇 1위 (최고 절약)', class: 'rank-1', medal: '🥇' },
-    { title: '🥈 2위', class: 'rank-2', medal: '🥈' },
-    { title: '🥉 3위', class: 'rank-3', medal: '🥉' }
+    { title: '🥇 1위 (최고 절약)', class: 'rank-1' },
+    { title: '🥈 2위', class: 'rank-2' },
+    { title: '🥉 3위', class: 'rank-3' }
   ];
 
   top3.forEach((entry, idx) => {
@@ -1503,7 +1503,6 @@ function renderTopSavingsRanking() {
 
     card.innerHTML = `
       <div class="rank-top-badge">
-        <span class="rank-medal">${rankInfo.medal}</span>
         <span class="rank-title">${rankInfo.title}</span>
       </div>
       <div class="rank-card-body">
